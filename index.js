40d7ca62e8202e2afd647bdd9b03c08f44902791
Grasu23 <haisanmatei@gmail.com>
2025-03-31 14:09:12 +0300

    modified:   index.html 	modified:   index.js 	new file:   resurse/css/eroare.css 	modified:   resurse/css/general.css 	new file:   resurse/json/erori.json 	new file:   views/fragmente/eroare.ejs

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 const express = require("express");
-
 const path = require("path");
+const fs = require("fs");
 
 app = express();
 
 app.set("view engine", "ejs");
+
+obGlobal = {
+    obErori: null
+}
+
+function initErori(){
+    let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
+    
+    obGlobal.obErori=JSON.parse(continut)
+    
+    obGlobal.obErori.eroare_default.imagine=path.join(obGlobal.obErori.cale_baza, obGlobal.obErori.eroare_default.imagine)
+    for (let eroare of obGlobal.obErori.info_erori){
+        eroare.imagine=path.join(obGlobal.obErori.cale_baza, eroare.imagine)
+    }
+    console.log(obGlobal.obErori)
+
+}
+
+initErori()
+
+function afisareEroare(res, identificator, titlu, text, imagine){
+    let eroare= obGlobal.obErori.info_erori.find(function(elem){ 
+                        return elem.identificator==identificator
+                    });
+    if(eroare){
+        if(eroare.status)
+            res.status(identificator)
+        var titluCustom=titlu || eroare.titlu;
+        var textCustom=text || eroare.text;
+        var imagineCustom=imagine || eroare.imagine;
+
+
+    }
+    else{
+        var err=obGlobal.obErori.eroare_default
+        var titluCustom=titlu || err.titlu;
+        var textCustom=text || err.text;
+        var imagineCustom=imagine || err.imagine;
+
+
+    }
+    res.render("pagini/eroare", { //transmit obiectul locals
+        titlu: titluCustom,
+        text: textCustom,
+        imagine: imagineCustom
+})
+
+}
 
 console.log("Calea proiectului: "+__dirname);
 console.log("Calea Fisierului index.js: "+__filename);
@@ -29,7 +77,6 @@ app.get(["/","/home","/index"],function(req,res){
 app.get("/despre",function(req,res){
     res.render("pagini/despre");
 });
-
 
 app.get("/fisier",function(req,res){
     res.sendFile(path.join(__dirname,"package.json"));
