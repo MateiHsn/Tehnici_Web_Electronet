062164b415b521248c24277a4df8e4d506a9c463
Grasu23 <haisanmatei@gmail.com>
2025-06-21 21:01:49 +0300

    modified:   index.html 	modified:   index.js 	modified:   resurse/css/eroare.css 	modified:   resurse/css/general.css 	modified:   resurse/css/nav.css 	modified:   resurse/css/nav_1000.css 	modified:   resurse/css/nav_700.css 	new file:   resurse/imagini/erori/interzis.png 	new file:   resurse/imagini/erori/lupa.png 	modified:   resurse/json/erori.json 	modified:   to_do.txt 	modified:   views/fragmente/head.ejs 	modified:   views/fragmente/header.ejs 	modified:   views/pagini/despre.ejs 	renamed:    views/fragmente/eroare.ejs -> views/pagini/eroare.ejs 	modified:   views/pagini/index.ejs 	modified:   views/pagini/produs.ejs 	modified:   views/pagini/produse.ejs

--- index.js
+++ index.js
@@ -1,155 +1,47 @@
-const express = require("express");
-const path = require("path");
+const express= require("express");
+const path= require("path");
 const fs = require("fs");
-const sharp = require("sharp");
-const sass = require("sass");
-const pg = require("pg");
 
-const Client=pg.Client;
 
-client=new Client({
-    database:"proiectelectronet",
-    user:"matei",
-    password:"matei",
-    host:"localhost",
-    port:5432
-})
+app= express();
 
-client.connect()
+v=[10,27,23,44,15]
 
-app = express();
+nrImpar=v.find(function(elem){return elem % 100 == 1})
+console.log(nrImpar)
+
+console.log("Folderul proiectului: ", __dirname)
+console.log("Calea fisierului index.js: ", __filename)
+console.log("Folderul curent de lucru: ", process.cwd())
 
 app.set("view engine", "ejs");
 
-app.get("/produse", function(req, res){
-    // console.log(req.query)
-    var conditieQuery=""; // TO DO where din parametri
-
-
-    queryOptiuni="select * from unnest(enum_range(null::categ_prajitura))"
-    client.query(queryOptiuni, function(err, rezOptiuni){
-        // console.log(rezOptiuni)
-
-
-        queryProduse="select * from prajituri"
-        client.query(queryProduse, function(err, rez){
-            if (err){
-                console.log(err);
-                afisareEroare(res, 2);
-            }
-            else{
-                res.render("pagini/produse", {produse: rez.rows, optiuni:rezOptiuni.rows})
-            }
-        })
-    });
-})
-
-obGlobal = {
-    obErori: null,
-    obImagini: null,
-    folderScss: path.join(__dirname+"/resurse/scss/"),
-    folderCss: path.join(__dirname+"/resurse/css/"),
-    folderBackup: path.join(__dirname+"backup")
+obGlobal={
+    obErori:null
 }
 
-// vect_foldere = ["temp", "backup", "temp1"];
-// for(let folder of vect_foldere){
-//     let cale_folder = path.join(__dirname, folder)
-//     if(!fs.existsSync()){
-//         fs.mkdirSync(cale_folder);
-//     }
-// }
 
-// clip video 
-// 1. curs HTML
-// taguri video / audio
-// varianta a 2-a 
-
-function initImagini(){
-    var continut= fs.readFileSync(path.join(__dirname,"resurse/json/galerie.json")).toString("utf-8");
-
-    obGlobal.obImagini=JSON.parse(continut);
-    let vImagini=obGlobal.obImagini.imagini;
-
-    let caleAbs=path.join(__dirname,obGlobal.obImagini.cale_galerie);
-    let caleAbsMediu=path.join(__dirname,obGlobal.obImagini.cale_galerie, "mediu");
-    if (!fs.existsSync(caleAbsMediu))
-        fs.mkdirSync(caleAbsMediu); 
-
-    for (let imag of vImagini){
-        [numeFis, ext]=imag.fisier.split(".");
-        let caleFisAbs=path.join(caleAbs,imag.fisier);
-        let caleFisMediuAbs=path.join(caleAbsMediu, numeFis+".webp");
-        sharp(caleFisAbs).resize(300).toFile(caleFisMediuAbs);
-        imag.fisier_mediu=path.join("/", obGlobal.obImagini.cale_galerie, "mediu",numeFis+".webp" )
-        imag.fisier=path.join("/", obGlobal.obImagini.cale_galerie, imag.fisier )
-        
-    }
-    // console.log(obGlobal.obImagini)
-}
-initImagini();
-
-function compileazaScss(caleScss, caleCss){
-    // console.log("cale:",caleCss);
-    if(!caleCss){
-
-        let numeFisExt=path.basename(caleScss); // intoarce numele fisierului cu extensie
-        let numeFis=numeFisExt.split(".")[0]   /// "a.scss"  -> ["a","scss"]
-        caleCss=numeFis+".css";
-    }
-    
-    if (!path.isAbsolute(caleScss))
-        caleScss=path.join(obGlobal.folderScss,caleScss )
-    if (!path.isAbsolute(caleCss))
-        caleCss=path.join(obGlobal.folderCss,caleCss )
-    
-
-    let caleBackup=path.join(obGlobal.folderBackup, "resurse/css");
-    if (!fs.existsSync(caleBackup)) {
-        fs.mkdirSync(caleBackup,{recursive:true})
-    }
-    
-    // la acest punct avem cai absolute in caleScss si  caleCss
-
-    let numeFisCss=path.basename(caleCss);
-    if (fs.existsSync(caleCss)){
-        fs.copyFileSync(caleCss, path.join(obGlobal.folderBackup, "resurse/css",numeFisCss ))// +(new Date()).getTime()
-    }
-    rez=sass.compile(caleScss, {"sourceMap":true});
-    fs.writeFileSync(caleCss,rez.css)
-    // console.log("Cale css:",caleCss);
-    // console.log("Compilare SCSS",rez);
-}
-//compileazaScss("a.scss");
-// la pornirea serverului
-vFisiere=fs.readdirSync(obGlobal.folderScss);
-for( let numeFis of vFisiere ){
-    if (path.extname(numeFis)==".scss"){
-        compileazaScss(numeFis);
+vect_foldere=["temp", "backup", "temp1"]
+for (let folder of vect_foldere ){
+    let caleFolder=path.join(__dirname,folder)
+    if (! fs.existsSync(caleFolder)){
+        fs.mkdirSync(caleFolder);
     }
 }
 
 
-fs.watch(obGlobal.folderScss, function(eveniment, numeFis){
-    // console.log(eveniment, numeFis);
-    if (eveniment=="change" || eveniment=="rename"){
-        let caleCompleta=path.join(obGlobal.folderScss, numeFis);
-        if (fs.existsSync(caleCompleta)){
-            compileazaScss(caleCompleta);
-        }
-    }
-})
 
 function initErori(){
     let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
-    
+    console.log(continut)
     obGlobal.obErori=JSON.parse(continut)
+    console.log(obGlobal.obErori)
     
     obGlobal.obErori.eroare_default.imagine=path.join(obGlobal.obErori.cale_baza, obGlobal.obErori.eroare_default.imagine)
     for (let eroare of obGlobal.obErori.info_erori){
         eroare.imagine=path.join(obGlobal.obErori.cale_baza, eroare.imagine)
     }
-    // console.log(obGlobal.obErori)
+    console.log(obGlobal.obErori)
 
 }
 
@@ -184,88 +76,94 @@ function afisareEroare(res, identificator, titlu, text, imagine){
 
 }
 
-console.log("Calea proiectului: "+__dirname);
-console.log("Calea Fisierului index.js: "+__filename);
-console.log("Calea folderului de lucru: "+process.cwd());
 
-app.get("/",function(req,res){
+
+app.use("/resurse", express.static(path.join(__dirname,"resurse")))
+app.get("/favicon.ico", function(req, res){
+    res.sendFile(path.join(__dirname, "resurse/imagini/favicon/favicon.ico"))
+})
+
+app.get(["/","/index","/home"], function(req, res){
+    res.render("pagini/index",{ip:req.ip});
+})
+
+// app.get("/despre", function(req, res){
+//     res.render("pagini/despre");
+// })
+
+app.get("/index/a", function(req, res){
     res.render("pagini/index");
-});
-
-// app.get("/index/a",function(req,res){
-//     res.render("pagini/index");
-// });
+})
 
 
-app.get(/^\/resurse\/[a-zA-Z1-9_\/]*$/,function(req,res,next){
+app.get("/cerere", function(req, res){
+    res.send("<p style='color:blue'>Buna ziua</p>")
+})
+
+
+app.get("/fisier", function(req, res, next){
+    res.sendfile(path.join(__dirname,"package.json"));
+})
+
+
+app.get("/abc", function(req, res, next){
+    res.write("Data curenta: ")
+    next()
+})
+
+app.get("/abc", function(req, res, next){
+    res.write((new Date())+"")
+    res.end()
+    next()
+})
+
+
+app.get("/abc", function(req, res, next){
+    console.log("------------")
+})
+
+
+
+app.get(/^\/resurse\/[a-zA-Z0-9_\/]*$/, function(req, res, next){
     afisareEroare(res,403);
 })
 
-app.use("/resurse",express.static(path.join(__dirname,"resurse")));
-app.use("/node_modules", express.static(path.join(__dirname,"node_modules")));
 
-
-app.get(["/","/home","/index"],function(req,res){
-    res.render("pagini/index", {ip:req.ip,imagini:obGlobal.obImagini.imag})
-});
-
-// app.get("/despre",function(req,res){
-//     res.render("pagini/despre");
-// });
-
-app.get("/fisier",function(req,res){
-    res.sendFile(path.join(__dirname,"package.json"));
-});
-
-// app.get("/cerere",function(req,res){
-//     res.send("<h1 style = 'color:green'>Buna seara!</h1>");
-// });
-
-// app.get("/abc",function(req,res,next){
-//     res.write("Data de azi: ");
-//     next();
-// });
-
-// app.get("/abc",function(req,res,next){
-//     res.write((new Date())+'');
-//     res.end();
-//     // next();
-// })
-
-app.get("/favicon.ico",function(req,res){
-    res.sendFile(path.join(__dirname,"resurse/imagini/favicon/favicon.ico"));
-});
-
-
-app.get("/*.ejs",function(req,res){
-    afisareEroare(400);
+app.get("/*.ejs", function(req, res, next){
+    afisareEroare(res,400);
 })
 
-app.get("/*",function(req,res,next){
-    try{
-        res.render("pagini"+req.url,function(err,rezultatRandare){
-        if(err){
-            if(err.message.startWith("Failed to lookup view")){
-                afisareEroare(res,err);
-            }else{
-                afisareEroare(res);
-            }
-        }else{
-            // console.log(rezultatRandare);
 
-        }
-    });
+app.get("/*", function(req, res, next){
+    try{
+        res.render("pagini"+req.url,function (err, rezultatRandare){
+            if (err){
+                if(err.message.startsWith("Failed to lookup view")){
+                    afisareEroare(res,404);
+                }
+                else{
+                    afisareEroare(res);
+                }
+            }
+            else{
+                console.log(rezultatRandare);
+                res.send(rezultatRandare)
+            }
+        });
     }
-    catch (errRandare){
-        // if(errRandare.message.startWith("Cannot find module ")){
-// 
-        // }
+    catch(errRandare){
+        if(errRandare.message.startsWith("Cannot find module")){
+            afisareEroare(res,404);
+        }
+        else{
+            afisareEroare(res);
+        }
     }
 })
 
 
 
 app.listen(8080);
-console.log("Serverul a pornit");
+console.log("Serverul a pornit")
 
 
